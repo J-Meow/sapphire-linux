2c99c77c1eefe4378a3e6ef89e8f3c0393a1ebaf
Jmeow <j@jmeow.net>
2025-06-19 10:01:01 -0500

    Change window title to Sapphire

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *button;
 
     window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Hello");
+    gtk_window_set_title(GTK_WINDOW(window), "Sapphire");
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
     button = gtk_button_new_with_label("Hello World");
